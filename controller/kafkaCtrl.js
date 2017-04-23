31ee18ecc1f13750965dc6a4d65f44070ee82a1a
calmar2 <m_calzetta@live.com>
2017-04-23 16:33:01 +0200

    fix reading from kafka

--- controller/kafkaCtrl.js
+++ controller/kafkaCtrl.js
@@ -24,41 +24,48 @@ function initKafkaFn() {
         ,'day_city_cons'
         ,'week_city_cons'
         ,'warning_state'
-        ,'median'];
+        ,'median'
+        ,'adjustment_data'];
+
     var kafkaTopics = [];
     for (var i = 0 ; i < topics.length ; i++){
         var topic = {};
         topic.topic = topics[i];
         topic.partition = 0;
+        topic.time = -1;
+        topic.maxNum = 1;
         kafkaTopics.push(topic)
     }
     var kafka = require('kafka-node'),
         Consumer = kafka.Consumer,
         client = new kafka.Client('localhost:2181'),
-        consumer = new Consumer(
+        offset = new kafka.Offset(client);
+
+    offset.fetch(kafkaTopics, function (err, data) {
+        var consumerTopics = [];
+        for (var i = 0 ; i < kafkaTopics.length; i++){
+            var topic = {};
+            topic.topic = kafkaTopics[i].topic;
+            topic.partition = 0;
+            topic.offset = data[topic.topic][0][0];
+            consumerTopics.push(topic);
+        }
+        var consumer = new Consumer(
             client,
-               kafkaTopics
-            ,
+            consumerTopics,
             {
-                autoCommit: false
+                autoCommit: false,
+                fromOffset: true,
             }
         );
-
-    consumer.on('message', function (message) {
-        console.log("topic : " + message.topic);
-        console.log("topic content: " + message.value);
-        socketCtrl.emitOnTopic(message.topic,message.value);
-
-
+        consumer.on('message', function (message) {
+            console.log("topic : " + message.topic);
+            console.log("topic content: " + message.value);;
+        });
+        consumer.on('error',function (error) {
+            console.log(error);
+        })
     });
-
-    consumer.on('error',function (error) {
-        console.log(error);
-    })
-
-
-
-
 
 
 }
